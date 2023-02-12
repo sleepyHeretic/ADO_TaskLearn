79ccdb7568d89c706967bd33d1f12b25a349b484
Lexie <lexie70rus@gmail.com>
2023-02-12 17:26:38 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace ADO_TaskLearn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.button_Save2 = new System.Windows.Forms.Button();
+            this.button_Save3 = new System.Windows.Forms.Button();
+            this.button_AllGroups = new System.Windows.Forms.Button();
+            this.button_AllStudents = new System.Windows.Forms.Button();
+            this.button_AllSubject = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(475, 272);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(695, 67);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(104, 38);
+            this.button_Save.TabIndex = 2;
+            this.button_Save.Text = "Сохранить изменения";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // button_Save2
+            // 
+            this.button_Save2.Location = new System.Drawing.Point(695, 131);
+            this.button_Save2.Name = "button_Save2";
+            this.button_Save2.Size = new System.Drawing.Size(104, 38);
+            this.button_Save2.TabIndex = 4;
+            this.button_Save2.Text = "Сохранить изменения";
+            this.button_Save2.UseVisualStyleBackColor = true;
+            this.button_Save2.Click += new System.EventHandler(this.button_Save2_Click);
+            // 
+            // button_Save3
+            // 
+            this.button_Save3.Location = new System.Drawing.Point(695, 201);
+            this.button_Save3.Name = "button_Save3";
+            this.button_Save3.Size = new System.Drawing.Size(104, 38);
+            this.button_Save3.TabIndex = 6;
+            this.button_Save3.Text = "Сохранить изменения";
+            this.button_Save3.UseVisualStyleBackColor = true;
+            this.button_Save3.Click += new System.EventHandler(this.button_Save3_Click);
+            // 
+            // button_AllGroups
+            // 
+            this.button_AllGroups.Location = new System.Drawing.Point(557, 67);
+            this.button_AllGroups.Name = "button_AllGroups";
+            this.button_AllGroups.Size = new System.Drawing.Size(108, 38);
+            this.button_AllGroups.TabIndex = 1;
+            this.button_AllGroups.Text = "Все группы";
+            this.button_AllGroups.UseVisualStyleBackColor = true;
+            this.button_AllGroups.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_AllStudents
+            // 
+            this.button_AllStudents.Location = new System.Drawing.Point(557, 131);
+            this.button_AllStudents.Name = "button_AllStudents";
+            this.button_AllStudents.Size = new System.Drawing.Size(108, 38);
+            this.button_AllStudents.TabIndex = 3;
+            this.button_AllStudents.Text = "Все студенты";
+            this.button_AllStudents.UseVisualStyleBackColor = true;
+            this.button_AllStudents.Click += new System.EventHandler(this.button_AllStudents_Click);
+            // 
+            // button_AllSubject
+            // 
+            this.button_AllSubject.Location = new System.Drawing.Point(557, 201);
+            this.button_AllSubject.Name = "button_AllSubject";
+            this.button_AllSubject.Size = new System.Drawing.Size(108, 38);
+            this.button_AllSubject.TabIndex = 5;
+            this.button_AllSubject.Text = "Все предметы";
+            this.button_AllSubject.UseVisualStyleBackColor = true;
+            this.button_AllSubject.Click += new System.EventHandler(this.button_AllSubject_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 543);
+            this.Controls.Add(this.button_Save3);
+            this.Controls.Add(this.button_AllSubject);
+            this.Controls.Add(this.button_Save2);
+            this.Controls.Add(this.button_AllStudents);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.button_AllGroups);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.Button button_Save2;
+        private System.Windows.Forms.Button button_Save3;
+        private System.Windows.Forms.Button button_AllGroups;
+        private System.Windows.Forms.Button button_AllStudents;
+        private System.Windows.Forms.Button button_AllSubject;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_TaskLearn
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataSet data = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(@"Data source = .\SQLEXPRESS; Initial Catalog = TaskLearnPlatform; Integrated security = true;");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            data.Tables.Clear();
+            adapter = new SqlDataAdapter("select * from Groups", connection);
+            adapter.Fill(data);
+            dataGridView1.DataSource = data.Tables[0];
+            
+        }
+
+        private void button_AllStudents_Click(object sender, EventArgs e)
+        {
+            data.Tables.Clear();
+            adapter = new SqlDataAdapter("select * from Students", connection);
+            adapter.Fill(data);
+            dataGridView1.DataSource = data.Tables[0];
+        }
+
+        private void button_AllSubject_Click(object sender, EventArgs e)
+        {
+            data.Tables.Clear();
+            adapter = new SqlDataAdapter("select * from Subjects", connection);
+            adapter.Fill(data);
+            dataGridView1.DataSource = data.Tables[0];
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string command = "Update Groups set GroupName = @pGroupName where id = @pId";
+                SqlCommand update = new SqlCommand(command, connection);
+
+                update.Parameters.Add(new SqlParameter("@pGroupName", SqlDbType.NVarChar, 50));
+                update.Parameters.Add(new SqlParameter("@pId", SqlDbType.Int));
+
+                update.Parameters["@pGroupName"].SourceVersion = DataRowVersion.Current;
+                update.Parameters["@pId"].SourceVersion = DataRowVersion.Current;
+
+                update.Parameters["@pGroupName"].SourceColumn = "GroupName";
+                update.Parameters["@pId"].SourceColumn = "Id";
+
+                adapter.UpdateCommand = update;
+                adapter.Update(data);
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button_Save2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string command = "Update Students set FirstName = @pFirstName, LastName = @pLastName, Group_FK = @pGroup_FK where id = @pId";
+                SqlCommand update = new SqlCommand(command, connection);
+
+                update.Parameters.Add(new SqlParameter("@pFirstName", SqlDbType.NVarChar, 50));
+                update.Parameters.Add(new SqlParameter("@pLastName", SqlDbType.NVarChar, 50));
+                update.Parameters.Add(new SqlParameter("@pGroup_FK", SqlDbType.Int));
+                update.Parameters.Add(new SqlParameter("@pId", SqlDbType.Int));
+
+                update.Parameters["@pFirstName"].SourceVersion = DataRowVersion.Current;
+                update.Parameters["@pLastName"].SourceVersion = DataRowVersion.Current;
+                update.Parameters["@pGroup_FK"].SourceVersion = DataRowVersion.Current;
+                update.Parameters["@pId"].SourceVersion = DataRowVersion.Current;
+
+                update.Parameters["@pFirstName"].SourceColumn = "FirstName";
+                update.Parameters["@pLastName"].SourceColumn = "LastName";
+                update.Parameters["@pGroup_FK"].SourceColumn = "Group_FK";
+                update.Parameters["@pId"].SourceColumn = "Id";
+
+                adapter.UpdateCommand = update;
+                adapter.Update(data);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button_Save3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string command = "Update Subjects set SubjectName = @pSubjectName where id = @pId ";
+                SqlCommand sqlCommand = new SqlCommand(command, connection);
+
+                sqlCommand.Parameters.Add(new SqlParameter("@pSubjectName", SqlDbType.NVarChar, 50));
+                sqlCommand.Parameters.Add(new SqlParameter("@pId", SqlDbType.Int));
+
+                sqlCommand.Parameters["@pSubjectName"].SourceVersion = DataRowVersion.Current;
+                sqlCommand.Parameters["@pId"].SourceVersion = DataRowVersion.Current;
+
+                sqlCommand.Parameters["@pSubjectName"].SourceColumn = "SubjectName";
+                sqlCommand.Parameters["@pId"].SourceColumn = "Id";
+
+                adapter.UpdateCommand = sqlCommand;
+                adapter.Update(data);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+    }
+}
